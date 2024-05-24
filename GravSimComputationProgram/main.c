c97c34982c23b10b96bb6586bfa6fe9b43a2f9e7
emerixx <juraj.svatek@gmail.com>
2024-05-24 12:33:12 +0200

    uploaded all the code

---
+++ GravSimComputationProgram/main.c
@@ -0,0 +1,228 @@
+#include <stdio.h>
+#include <math.h>
+#include <sys/stat.h>
+#include <time.h>
+
+//program setup
+char outputFolderName[] = "../outputTxtFiles";
+
+double positions; // number of lines in an object's file. The lenght of the simulation
+double timeDelta; // time step every loop, accuracy of the simulation. lower number -> higher accuracy
+    
+int logsPerComputation; // how many lines are written to the console during the computation (info about progress)
+int compress; // write only every {compress} positions to reduce file size. when decreaseing timeDelta, its recommended to increase this
+    
+//constants constants
+double gravityConstant = 39.4947001202; // AU^3 MO^-1 Year^-2
+
+int NofBodies;
+
+clock_t start, curTime, end;
+
+  
+  
+
+
+  
+
+
+ 
+
+
+typedef struct body{
+  double mass;
+  double position[2];
+  double velocity[2];
+  double acceleration[2];
+  double accelerationOld[2];
+  FILE *bodyFile;
+} body ;
+
+
+
+void createOutputFolder(){
+  
+  mkdir(outputFolderName, 0777);
+ 
+  
+}
+
+
+void saveInitialConditions(body bodies[]){
+  FILE *initCon;
+  char path[30]; 
+  sprintf(path,"%s/initialConditions.txt", outputFolderName);
+  initCon = fopen(path, "w");
+  
+  //save initial masses
+  fprintf(initCon, "----------Masses---------\n");
+  for (int i = 0; i < NofBodies; i++){
+    char str[30];
+    sprintf(str, "mass (body %d): %f;\n", i, bodies[i].mass );
+    fprintf(initCon, "%s", str);
+  }
+
+  //save initial positions
+  fprintf(initCon, "\n----------Positions---------\n");
+  for (int i = 0; i < NofBodies; i++){
+    char str[45];
+    sprintf(str, "position (body %d): {%f, %f};\n", i, bodies[i].position[0], bodies[i].position[1]);
+    fprintf(initCon, "%s", str);
+  }
+
+  //save initial velocities
+  fprintf(initCon, "\n----------Velocities---------\n");
+  for (int i = 0; i < NofBodies; i++){
+    char str[45];
+    sprintf(str, "velocity (body %d): {%f, %f};\n", i, bodies[i].velocity[0], bodies[i].velocity[1]);
+    fprintf(initCon, "%s", str);
+  }
+  fclose(initCon);
+
+}
+
+void createBodyFiles(body bodies[]) {
+
+  char filename[20]; // Increased size to accommodate folder name
+  
+
+  for (int i = 0; i < NofBodies; i++) {
+    sprintf(filename, "%s/%d.txt", outputFolderName, i);
+    bodies[i].bodyFile = fopen(filename, "w");
+    
+  
+  }
+}
+
+void computeAndWrite(int loop, body bodies[]){
+  for (int i = 0; i < NofBodies; i++){
+    double force[] = {0, 0};
+    
+    for (int j = 0; j < NofBodies; j++){
+      
+      if(j == i){
+        continue;
+      }
+      double distanceAsVector[] = {0, 0};
+      double forceThis[] = {0, 0};
+      double distance = 0;
+
+      distanceAsVector[0] = bodies[i].position[0] - bodies[j].position[0];
+      distanceAsVector[1] = bodies[i].position[1] - bodies[j].position[1];
+      
+      distance = sqrt( pow(distanceAsVector[0], 2) + pow(distanceAsVector[1], 2) );
+
+      forceThis[0] = -(gravityConstant * bodies[i].mass * bodies[j].mass * distanceAsVector[0]) / pow(distance, 3);
+      forceThis[1] = -(gravityConstant * bodies[i].mass * bodies[j].mass * distanceAsVector[1]) / pow(distance, 3);
+      
+
+      
+   
+      
+      force[0] += forceThis[0];
+      force[1] += forceThis[1];
+
+
+    }
+
+    bodies[i].position[0] += bodies[i].velocity[0] * timeDelta + bodies[i].acceleration[0] / 2 * pow(timeDelta, 2);
+    bodies[i].position[1] += bodies[i].velocity[1] * timeDelta + bodies[i].acceleration[1] / 2 * pow(timeDelta, 2);
+    
+    bodies[i].accelerationOld[0] = bodies[i].acceleration[0];
+    bodies[i].accelerationOld[1] = bodies[i].acceleration[1];
+
+    bodies[i].acceleration[0] = force[0] / bodies[i].mass;
+    bodies[i].acceleration[1] = force[1] / bodies[i].mass;
+
+    bodies[i].velocity[0] += (bodies[i].acceleration[0] + bodies[i].accelerationOld[0]) / 2 * timeDelta;
+    bodies[i].velocity[1] += (bodies[i].acceleration[1] + bodies[i].accelerationOld[1]) / 2 * timeDelta;
+    
+    if (loop % compress == 0){
+      char out[50] = {0};
+      sprintf(out, "%f,%f\n", bodies[i].position[0], bodies[i].position[1]);
+      
+      
+      fprintf(bodies[i].bodyFile, "%s", out);
+    }
+
+    
+
+    
+  }
+  if (loop % (int)(positions / logsPerComputation) == 0){
+    double done = loop / positions;
+    curTime = clock();
+    double timeElapsed =(double)(curTime - start) / (double)(CLOCKS_PER_SEC);
+    double approxTimeLeft = timeElapsed / done - timeElapsed;
+    printf("Done : %f %%; Time Elapsed : %f s; approx time left : %f s; \n", done*100, timeElapsed, approxTimeLeft);
+  }
+}
+
+int main(){
+  
+
+ 
+  start = clock();
+  
+  //specify initial conditions
+  double massInit[] = { 1, 1, 1, 1, 1 };
+  double posInit[][2] =
+  {
+    {2, -2},
+    {0.74, 2},
+    {-1, -1},
+    {1, -0.75},
+    {1, 0.88}
+  };
+  double velInit[][2] =
+  {
+    {-2.7, 0.7},
+    {2.25, 2},
+    {3.75, 0.25},
+    {1.7, 0},
+    {-1.83, 1.25}
+
+
+  };
+  NofBodies = sizeof(massInit) / sizeof(massInit[0]);
+
+  body bodies[NofBodies]; 
+  
+  for(int i = 0; i < NofBodies; i++){
+    bodies[i].mass = massInit[i];
+    for(int j = 0; j < 2; j++){
+      
+      bodies[i].position[j] = posInit[i][j];
+      bodies[i].velocity[j] = velInit[i][j];
+    }
+  }
+
+
+  positions = pow(10, 8);
+  timeDelta = pow(10, -8);
+  logsPerComputation = pow(10, 2);
+  compress = pow(10, 4);
+
+  createOutputFolder();
+ 
+  saveInitialConditions(bodies);
+  
+  printf("saved initial conditions\n");
+
+  createBodyFiles(bodies);
+  
+  for (int i = 0; i < positions; i++){
+    
+    computeAndWrite(i, bodies);
+  }
+
+  end = clock();
+
+  double time_taken = (double)(end - start) / (double)(CLOCKS_PER_SEC);
+  
+  printf("Time taken: %f s\n", time_taken);
+
+  return 0;
+
+
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+This is a Project ive been working on for a while now, its a program that simulates gravitational interaction between N bodies.
+Whole project is written in C and uses raylib library for visualization ( https://github.com/raysan5/raylib ).
+The project is split into 2 parts:
+
+1. Computation Module (The brain):
+    This part calculates the positions of the bodies during the simulation.
+    It currently uses Velocity verlet for solving ODEs (although i might use RFK45 in the near future).
+    (The code isnt optimized very well, so there may be few bugs)
+
+2. Graphical Visualizer (The thing that converts long .txt files into pretty animations and/or images):
+    This part reads the .txt files with the positions and converts them into (pretty) animations and/or images.
+    It utilizes the raylib library ( https://github.com/raysan5/raylib ) to visualize the data.
+    (Also isnt very optimized)
+
+Any suggestions on how to make the code faster and/or less buggy (no bugs present to my knowledge) are welcome.
+The folder "outputTxtFiles" contains output of a short 5 body problem (it took abt 5 mins to compute positions)
